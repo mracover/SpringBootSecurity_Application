554ef9e46c79c61aa06bb746e0bfd8cff76ea2d1
Egor Davydov <mracover@gmail.com>
2022-11-11 18:04:19 +0500

    Lazy fetch

--- src/main/java/ru/kata/spring/boot_security/demo/dao/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleRepository.java
@@ -1,7 +1,5 @@
 package ru.kata.spring.boot_security.demo.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.entity.Role;
 
 import java.util.ArrayList;
